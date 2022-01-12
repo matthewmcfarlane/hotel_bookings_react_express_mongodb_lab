5c212b5ce8e0379269cd4c9da880d459aac9d506
Jack Cogan <jackcogan97@y7mail.com>
2022-01-12 13:11:44 +0000

    Added: Server.js setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+app.use(express.json());
+
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('hotel');
+    const bookingsCollection = db.collection('bookings');
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use('/api/bookings', bookingsRouter);
+  })
+  .catch(console.err);
+
+  app.listen(5000, function() {
+    console.log(`Server is running on port ${ this.address().port }`);
+  })
